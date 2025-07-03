761280ef3b5dcfd89d1386fe5e54561d0bdb5c3c
Shivam Shukla <shivamshukla211193@gmail.com>
2025-07-04 00:02:12 +0530

    Add handling for the conjured by mana based upon test files

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,7 +10,10 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        if item.name == "Conjured Mana Cake":
+                            item.quality = max(0, item.quality - 2)
+                        else:
+                            item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -28,7 +31,10 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                if item.name == "Conjured Mana Cake":
+                                    item.quality = max(0, item.quality - 2)
+                                else:
+                                    item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
